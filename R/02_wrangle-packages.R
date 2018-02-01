e2015d2cf73f706944b1f89bd536c4e3b2ad7b1d
LuisD <luisd@ciencias.unam.mx>
2018-02-01 11:46:12 -0800

    modified script, wrote output csvs

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,5 +1,9 @@
 ## create a data frame from data/installed-packages.csv
 ## with, e.g., readr::read_csv() or read.csv()
+library(here)
+library(tidyverse)
+
+ipt <- read.csv(here("data","installed-packages.csv"),stringsAsFactors = FALSE)
 
 ## filter out packages in the default library
 ## keep variables Package and Built
@@ -9,6 +13,7 @@ apt <- ipt %>%
   select(Package, Built)
 
 ## write this new, smaller data frame to data/add-on-packages.csv
+write.csv(apt,here("data","add-on-packages.csv"),row.names = FALSE)
 
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
@@ -17,9 +22,8 @@ apt_freqtable <- apt %>%
   mutate(prop = n / sum(n))
 
 ## write this data frame to data/add-on-packages-freqtable.csv
-## YES overwrite the files that are there now
-## they came from me (Jenny)
-## they are just examples
+write.csv(apt_freqtable,here("data","add-on-packages-freqtable.csv"),row.names = FALSE)
+
 
 ## when this script works, stage & commit it and the csv files
 ## PUSH!
